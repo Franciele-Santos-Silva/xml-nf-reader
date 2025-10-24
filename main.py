12f1d448895613c1ecdbd9149880f5edbc253a70
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-10-24 17:13:38 -0300

    Teste de saída

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ def pegar_infos(nome_arquivo):
     print(f'Arquivo: {nome_arquivo}')
     with open(f'nfs/{nome_arquivo}', 'rb') as arquivo_xml:
         dic_arquivo = xmltodict.parse(arquivo_xml)
-        print(json.dumps(dic_arquivo))
-        infos_nf = dic_arquivo[}"NFe"]['infNFe']
+        # print(json.dumps(dic_arquivo))
+        infos_nf = dic_arquivo["NFe"]['infNFe']
+        numero_nota = infos_nf['@Id']
+        empresa_emissora = infos_nf['emit']['xNome']
+        nome_cliente = infos_nf['dest']['xNome']
+        endereco = infos_nf['dest']['enderDest']
+        peso =  infos_nf['transp']['vol']['pesoB']
+        print(numero_nota, empresa_emissora, nome_cliente, endereco, peso, sep='\n')
 
 
 
