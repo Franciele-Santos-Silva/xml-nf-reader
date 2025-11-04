dd2f6a2a0addf97e046bd1e2ff9fb588bbf2669d
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-11-04 16:26:59 -0300

    Organizando peso

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ def pegar_infos(nome_arquivo):
                  infos_nf = dic_arquivo["NFe"]['infNFe']
             else:
                  infos_nf = dic_arquivo["nfeProc"]['NFe']['infNFe']
-
-            infos_nf = dic_arquivo["NFe"]['infNFe']
             numero_nota = infos_nf['@Id']
             empresa_emissora = infos_nf['emit']['xNome']
             nome_cliente = infos_nf['dest']['xNome']
             endereco = infos_nf['dest']['enderDest']
-            peso =  infos_nf['transp']['vol']['pesoB']
+            if "vol" in infos_nf['transp']:
+                peso =  infos_nf['transp']['vol']['pesoB']
+            else:
+                peso = '0'
             print(numero_nota, empresa_emissora, nome_cliente, endereco, peso, sep='\n')
         except Exception as e:
             print(f'ERRO ENCONTRADO {e}')
