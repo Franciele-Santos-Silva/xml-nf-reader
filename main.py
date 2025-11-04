70ece3082b95f67c1c5767093d7f41aebe8407ea
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-11-04 16:37:34 -0300

    nem sei o que escrever

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 import xmltodict
 import os
-import json
 
 def pegar_infos(nome_arquivo):
     print(f'Pegou as informações: {nome_arquivo}')
     with open(f'nfs/{nome_arquivo}', 'rb') as arquivo_xml:
         dic_arquivo = xmltodict.parse(arquivo_xml)
 
-        try: 
-            if 'NFe' in dic_arquivo:
+        if 'NFe' in dic_arquivo:
                  infos_nf = dic_arquivo["NFe"]['infNFe']
-            else:
+        else:
                  infos_nf = dic_arquivo["nfeProc"]['NFe']['infNFe']
-            numero_nota = infos_nf['@Id']
-            empresa_emissora = infos_nf['emit']['xNome']
-            nome_cliente = infos_nf['dest']['xNome']
-            endereco = infos_nf['dest']['enderDest']
-            if "vol" in infos_nf['transp']:
+        numero_nota = infos_nf['@Id']
+        empresa_emissora = infos_nf['emit']['xNome']
+        nome_cliente = infos_nf['dest']['xNome']
+        endereco = infos_nf['dest']['enderDest']
+        if "vol" in infos_nf['transp']:
                 peso =  infos_nf['transp']['vol']['pesoB']
-            else:
+        else:
                 peso = '0'
-            print(numero_nota, empresa_emissora, nome_cliente, endereco, peso, sep='\n')
-        except Exception as e:
-            print(f'ERRO ENCONTRADO {e}')
-            print(json.dumps(dic_arquivo))
-
-
-
-
+        print(numero_nota, empresa_emissora, nome_cliente, endereco, peso, sep='\n')
 
 lista_arquivos = os.listdir('nfs')
+
 for arquivo in lista_arquivos:
           pegar_infos(arquivo)
     # break
