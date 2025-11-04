85b345b09b3fbc19b1ba64e9232f191027d81b10
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-11-04 17:11:25 -0300

    NFs

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ def pegar_infos(nome_arquivo, valores):
         dic_arquivo = xmltodict.parse(arquivo_xml)
 
         if 'NFe' in dic_arquivo:
-                 infos_nf = dic_arquivo["NFe"]['infNFe']
+            infos_nf = dic_arquivo["NFe"]['infNFe']
         else:
-                 infos_nf = dic_arquivo["nfeProc"]['NFe']['infNFe']
+            infos_nf = dic_arquivo["nfeProc"]['NFe']['infNFe']
+        
         numero_nota = infos_nf['@Id']
         empresa_emissora = infos_nf['emit']['xNome']
         nome_cliente = infos_nf['dest']['xNome']
         endereco = infos_nf['dest']['enderDest']
+
         if "vol" in infos_nf['transp']:
-                peso =  infos_nf['transp']['vol']['pesoB']
+            peso =  infos_nf['transp']['vol']['pesoB']
         else:
-                peso = '0'
+            peso = '0'
         valores.append([numero_nota, empresa_emissora, nome_cliente, endereco, peso])
 
 lista_arquivos = os.listdir('nfs')
@@ -27,7 +29,7 @@ colunas = ["numero_nota", "empresa_emissora", "nome_cliente", "endereco", "peso"
 valores = []
 
 for arquivo in lista_arquivos:
-          pegar_infos(arquivo, valores)
+    pegar_infos(arquivo, valores)
 
 tabela = pd.DataFrame(columns= colunas, data= valores)
 tabela.to_excel('Notas_Fiscais.xlsx', index = False)
